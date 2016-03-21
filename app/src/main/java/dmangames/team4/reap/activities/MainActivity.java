a4e4f1465584328d3e8c2c21f1863a5088fb05ac
Brian Wang <b.wang@utexas.edu>
2016-03-21 13:53:46 -0500

    Amend project files

---
+++ app/src/main/java/dmangames/team4/reap/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package dmangames.team4.reap.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dmangames.team4.reap.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
