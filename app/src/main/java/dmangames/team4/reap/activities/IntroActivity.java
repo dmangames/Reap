be1915aba69b46a5192f1ebe7d541746167ba214
Andrew Huang <andrewhuang@utexas.edu>
2016-05-01 17:18:19 -0500

    Added break slide to tutorial

--- app/src/main/java/dmangames/team4/reap/activities/IntroActivity.java
+++ app/src/main/java/dmangames/team4/reap/activities/IntroActivity.java
@@ -20,9 +20,10 @@ public class IntroActivity extends AppIntro2 {
     public void init(@Nullable Bundle savedInstanceState) {
         int sandColor = ContextCompat.getColor(getApplicationContext(), R.color.sand);
         addSlide(AppIntroFragment.newInstance("Welcome to Reap", "Keep track of your life", R.drawable.reap_icon_splash, sandColor, Color.BLACK, Color.BLACK));
-        addSlide(AppIntroFragment.newInstance("Custom Activities", "Create activities specific to your needs", R.drawable.icons, sandColor, Color.BLACK, Color.BLACK));
+        addSlide(AppIntroFragment.newInstance("Custom Activities", "Create custom activities specific to your needs with an array of built-in icons", R.drawable.icons, sandColor, Color.BLACK, Color.BLACK));
         addSlide(AppIntroFragment.newInstance("Timers", "Use either a simple timer or the Pomodoro timer", R.drawable.timers, sandColor, Color.BLACK, Color.BLACK));
-        addSlide(AppIntroFragment.newInstance("Progress", "Track your progress for each activity", R.drawable.jars, sandColor, Color.BLACK, Color.BLACK));
+        addSlide(AppIntroFragment.newInstance("Breaks", "Easily pause your current activity and change to a break", R.drawable.breaks, sandColor, Color.BLACK, Color.BLACK));
+        addSlide(AppIntroFragment.newInstance("Progress", "Track your progress for each activity. Each icon or \"jar\" represents an hour of work", R.drawable.jars, sandColor, Color.BLACK, Color.BLACK));
         addSlide(AppIntroFragment.newInstance("You are all set. Enjoy Reap.", "Get Productive!", R.drawable.check, sandColor, Color.BLACK, Color.BLACK));
         setIndicatorColor(Color.WHITE, Color.GRAY);
     }
